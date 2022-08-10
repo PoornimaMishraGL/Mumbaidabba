299cd6086c02533a2e617d0186ad431fd012d9e8
hemrajlkragl <hemraj.lakhara@globallogic.com>
2022-08-10 18:58:16 +0530

    Login registeration page modified and user cart access authentication done

---
+++ Mumbaidabba/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mumbaidabba.Areas.Identity.Data
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(50)")]
+        public string FirstName { get; set; }
+        [PersonalData]
+        [Column(TypeName = "nvarchar(50)")]
+        public string LastName { get; set; }
+    }
+}
